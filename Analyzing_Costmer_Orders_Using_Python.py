1539d46c1fcd009abab4fb247a259bdbfd0f3671
Micah Borycz <mborycz.borycz@gmail.com>
2026-05-03 12:05:02 -0400

    Added the second step to this problem. Used a dictionary to map each projuct to its respective caegory. Created a set of unique product categories.

--- Analyzing_Costmer_Orders_Using_Python.py
+++ Analyzing_Costmer_Orders_Using_Python.py
@@ -26,3 +26,23 @@ for name, product, price, category in orders:
     
 for key, value in customer_orders.items():
     print(f"This customer's name is: {key} they ordered {value}")
+
+# Task 2: Map products to categories and find unique categories
+
+# Use a dictionary to map each product to its respective category
+categorized_products = {}
+for name, product, price, category in orders:
+    if product not in categorize_products:
+        categorized_products[product] = []
+    categorized_products[product].append(category)
+    
+for key, value in categorized_products.items():
+    print(f" This is the item: {key} and this is it's catagory: {value}")
+
+# Create a set of unique product categories
+unique_categories = set()
+for name, product, price, category in orders:
+    if category not in unique_categories:
+        unique_categories.add(category)
+
+print(f"\nAll availabel product categories are: {unique_categories}")
